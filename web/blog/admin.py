e19c969447d56a0dddff946bbc32261bb846cc34
gryroach <gryroach@gmail.com>
2022-04-15 23:19:58 +0300

    Created models and admin

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article, Comment
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'article')
--- web/blog/models.py
+++ web/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField("Title", max_length=255, blank=False)
+    content = models.TextField("Content", blank=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        ordering = ['title']
+
+
+class Comment(models.Model):
+    owner = models.CharField("Name", max_length=255, blank=False)
+    created = models.DateTimeField("Created", auto_now_add=True)
+    text = models.TextField("Text", blank=True)
+    article = models.ForeignKey(Article, related_name="comment", on_delete=models.CASCADE, blank=False)
+    related_comment = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id} {self.owner} {self.created}"
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+        ordering = ['created']
